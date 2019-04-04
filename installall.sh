715406a86b8a3827e00b12ffb90951a0a0ab544e
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-04-04 03:06:30 -0500

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -161,5 +161,9 @@ chown -R "$DOMAINUSER":"$DOMAINUSER" ./main_server
 rm *zip
 
 echo "Setup completed successfully!!"
+echo "make note of the following info to set up the BMLT root server"
+echo "BMLT database: $BMLTDB"
+echo "BMLT database user: $DOMAINUSER"
+echo "BMLT database password: $PASSWD" 
 #Reboot system
 #reboot
